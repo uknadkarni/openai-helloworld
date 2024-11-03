9989d52c1aefa8c37fb2c23b152531cf52153dee
uknadkarni <10155870+uknadkarni@users.noreply.github.com>
2024-11-03 16:38:42 -0500

    Create AIController.java

---
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/AIController.java
@@ -0,0 +1,28 @@
+package org.springframework.ai.openai.samples.helloworld;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+class AIController {
+	private final ChatClient chatClient;
+
+	@Autowired
+	AIController(ChatClient chatClient) {
+		this.chatClient = chatClient;
+	}
+	@GetMapping("/ai")
+	Map<String, String> completion(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+		return Map.of(
+				"completion",
+				chatClient.prompt()
+						.user(message)
+						.call()
+						.content());
+	}
+}
